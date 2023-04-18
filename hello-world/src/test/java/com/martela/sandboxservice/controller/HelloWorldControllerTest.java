b9f5dde5930c186b9ed34e176f5af0b87a00ceb9
kacper <kacper99@hotmail.co.uk>
2023-04-18 20:08:10 +0100

    feat: add hello by name

--- hello-world/src/test/java/com/martela/sandboxservice/controller/HelloWorldControllerTest.java
+++ hello-world/src/test/java/com/martela/sandboxservice/controller/HelloWorldControllerTest.java
@@ -1,15 +1,23 @@
 package com.martela.sandboxservice.controller;
 
+import com.martela.sandboxservice.service.HelloService;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
 
+import static com.martela.sandboxservice.controller.HelloWorldController.*;
+import static org.assertj.core.api.Assertions.*;
 
 class HelloWorldControllerTest {
 
     @Test
     void shouldReturnHelloWorld() {
-        HelloWorldController helloWorldController = new HelloWorldController();
+        HelloWorldController helloWorldController = new HelloWorldController(new HelloService());
         assertThat(helloWorldController.helloWorld()).isEqualTo("Hello World!");
+    }
+
+    @Test
+    void shouldReturnHelloName() {
+        HelloWorldController helloWorldController = new HelloWorldController(new HelloService());
+        assertThat(helloWorldController.helloName(new HelloNameRequest("John"))).isEqualTo("Hello John!");
     }
 
 }
