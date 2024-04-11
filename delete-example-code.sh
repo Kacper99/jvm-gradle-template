d5c80b8401d1a020eebc7452b5e15f32c6841811
Kacper Martela <kacper99@hotmail.co.uk>
2024-04-11 21:42:52 +0100

    feat: script to delete example code

---
+++ delete-example-code.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Delete the application folder
+rm -rf application
+
+# Delete the library folder
+rm -rf library
+
+# Remove the project includes in settings.gradle.kts
+sed -i '' '/include("application")/d' settings.gradle.kts
+sed -i '' '/include("library")/d' settings.gradle.kts
