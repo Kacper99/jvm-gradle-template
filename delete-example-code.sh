3c8be81f288c6838b53eaf77f719d980d369f94a
Kacper Martela <kacper99@hotmail.co.uk>
2024-04-11 21:47:38 +0100

    feat: remove file once cleaned up

--- delete-example-code.sh
+++ delete-example-code.sh
@@ -9,3 +9,6 @@ rm -rf library
 # Remove the project includes in settings.gradle.kts
 sed -i '' '/include("application")/d' settings.gradle.kts
 sed -i '' '/include("library")/d' settings.gradle.kts
+
+# Remove the file itself
+rm delete-example-code.sh
